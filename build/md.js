07f3ebd7084b6b05f626724f22fb748084d217ba
AnimalInstinct <animalinstnct@protonmail.com>
2022-09-11 11:03:48 +0700

    cleanup

--- build/md.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-// const fs = require('fs');
-// const  MarkdownIt = require("markdown-it")
-// const markdown = MarkdownIt({
-//     html: true
-// })
-// export { markdown }
-//# sourceMappingURL=data:application/json;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoibWQuanMiLCJzb3VyY2VSb290IjoiIiwic291cmNlcyI6WyIuLi9zcmMvbWQudHMiXSwibmFtZXMiOltdLCJtYXBwaW5ncyI6IjtBQUFBLDRCQUE0QjtBQUM1Qiw2Q0FBNkM7QUFFN0MsZ0NBQWdDO0FBQ2hDLGlCQUFpQjtBQUNqQixLQUFLO0FBR0wsc0JBQXNCIn0=
